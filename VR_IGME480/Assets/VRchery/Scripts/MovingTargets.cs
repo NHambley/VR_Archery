6e283bb639d73abc0a01cd7fe1dafd84eaaa8676
NHambley <nhambley6@gmail.com>
2019-04-05 22:46:09 -0400

    Started work building the scene and working on scripts for different target movements

---
+++ VR_IGME480/Assets/VRchery/Scripts/MovingTargets.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTargets : MonoBehaviour
+{
+    // an array of the different positions for the targets to move through
+    public GameObject[] movePos;
+    int step = 0;
+
+    Vector3 position;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveVec = (movePos[step].transform.position - position);
+        moveVec.Normalize();
+        moveVec *= (Time.deltaTime * speed);
+
+        position += moveVec;
+        transform.position = position;
+
+        if(Vector3.Distance(transform.position, movePos[step].transform.position) < 0.5f)
+        {
+            step += 1;
+
+            // make sure that step does not go outside the array bounds
+            if(step == movePos.Length)
+            {
+                step = 0;
+            }
+        }
+    }
+}
