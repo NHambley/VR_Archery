aff596c83dc31f00097a3d4e4c5dad456f501760
Mitch Steffens (RIT Student) <mds4685@ad.rit.edu>
2019-04-07 14:51:20 -0400

    Very basics, very buggy

---
+++ VR_IGME480/Assets/Scripts/Arrow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Arrow : MonoBehaviour
+{
+    public SteamVR_ActionSet m_ActionSet;
+
+    public SteamVR_Action_Boolean m_SingleAction;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ArrowManager.Instance.AttachBowToArrow();
+        AttachArrow();
+    }
+
+    private void AttachArrow()
+    {
+        m_SingleAction = SteamVR_Actions._default.GrabPinch;
+        m_SingleAction.GetStateDown(SteamVR_Input_Sources.Any);
+
+        //SteamVR_Actions._default.Squeeze.Get
+    }
+}
---
+++ VR_IGME480/Assets/Scripts/ArrowManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ArrowManager : MonoBehaviour
+{
+    public static ArrowManager Instance;
+
+    public SteamVR_Behaviour_Pose trackedObj;
+
+    private GameObject currentArrow;
+
+    public GameObject stringAttachPoint;
+    public GameObject arrowStartPoint;
+
+    public GameObject arrowPrefab;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if(Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AttachArrow();
+    }
+
+    private void AttachArrow()
+    {
+        if(currentArrow == null)
+        {
+            currentArrow = Instantiate(arrowPrefab);
+            currentArrow.transform.parent = trackedObj.transform;
+            currentArrow.transform.localPosition = new Vector3(0f, 0f, 0.342f);
+        }
+    }
+
+    public void AttachBowToArrow()
+    {
+        currentArrow.transform.parent = stringAttachPoint.transform;
+        currentArrow.transform.localPosition = arrowStartPoint.transform.localPosition;
+        currentArrow.transform.rotation = arrowStartPoint.transform.rotation;
+    }
+}
