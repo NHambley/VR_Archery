bc8f63344236965dc377b11c1b8f1deeeb14926c
Mitch Steffens (RIT Student) <mds4685@ad.rit.edu>
2019-04-10 12:51:22 -0400

    Fixes & Bird Script
    Did a couple fixes to the scene heirarchy and added a script for "birds" that fly around the scene.

---
+++ VR_IGME480/Assets/VRchery/Scripts/Bird.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    Vector3 position;
+
+    public float turnSpeed = 2.0f;
+    public float speed = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position += this.transform.forward * speed;
+        this.transform.Rotate(Vector3.up, turnSpeed);
+
+        this.transform.position = position;
+    }
+}
