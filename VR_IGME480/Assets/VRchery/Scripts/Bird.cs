4f95725c42bcdaefc2210a690bfffce934e93a31
Mitch Steffens (RIT Student) <mds4685@ad.rit.edu>
2019-04-12 12:42:34 -0400

    Oscillation and Scene tweaks
    Add a sinusoidal motion to the bird targets, and adjusted some objects in the scene.

--- VR_IGME480/Assets/VRchery/Scripts/Bird.cs
+++ VR_IGME480/Assets/VRchery/Scripts/Bird.cs
@@ -9,10 +9,20 @@ public class Bird : MonoBehaviour
     public float turnSpeed = 2.0f;
     public float speed = 0.05f;
 
+    float baseHeight;
+    float degree;
+
+    public float degreesPerSecond = 180f;
+    public float moveheight = 0.5f;
+
+    public bool oscillate = false;
+
     // Start is called before the first frame update
     void Start()
     {
         position = this.transform.position;
+        degree = 0.0f;
+        baseHeight = this.transform.position.y;
     }
 
     // Update is called once per frame
@@ -21,6 +31,21 @@ public class Bird : MonoBehaviour
         position += this.transform.forward * speed;
         this.transform.Rotate(Vector3.up, turnSpeed);
 
+        if(oscillate)
+            OscillateHeight();
+
         this.transform.position = position;
+    }
+
+    void OscillateHeight()
+    {
+        if(degree > 359.0f)
+        {
+            degree = 0f;
+        }
+        float siny = Mathf.Sin(degree) * moveheight;
+        position.y = baseHeight + siny;
+
+        degree += (degreesPerSecond / 60.0f) * Time.deltaTime;
     }
 }
