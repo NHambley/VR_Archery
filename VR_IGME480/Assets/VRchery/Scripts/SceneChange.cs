66916bff027186628ee768c4ad36ed810330deb8
NHambley <nhambley6@gmail.com>
2019-04-28 19:30:40 -0400

    Added a main menu

---
+++ VR_IGME480/Assets/VRchery/Scripts/SceneChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChange : MonoBehaviour
+{
+    public string lvlName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "projectile")
+        {
+            SceneManager.LoadScene(lvlName);
+        }
+    }
+}
